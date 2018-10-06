3d098b3072904d1776d971b3001d42a6c8ebea6a
khoitd1997 <khoidinhtrinh@gmail.com>
2018-10-06 13:10:44 -0700

    added c++ guard for segger debug

--- debug_utils/swo_segger.h
+++ debug_utils/swo_segger.h
@@ -3,6 +3,10 @@
 #ifndef _SWO_SEGGER_H
 #define _SWO_SEGGER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void SWO_PrintChar(char c);
 void SWO_PrintString(const char *s);
 
@@ -14,5 +18,9 @@ void SWO_PrintString(const char *s);
 #define ITM_STIM_U8 (*(volatile char *)0xE0000000)           // Stimulus Port 0 Register byte acces
 #define ITM_ENA (*(volatile unsigned int *)0xE0000E00)       // Trace Enable Ports Register
 #define ITM_TCR (*(volatile unsigned int *)0xE0000E80)       // Trace control register
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
